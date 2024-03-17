5bf8a225c8369b47d07986e1701df5df827da6c9
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-03-17 00:00:53 -0500

    a

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,18 +1,28 @@
 package gestion;
 
+import java.util.ArrayList;
+
 import zooAnimales.Animal;
 
 public class Zona {
 	private String nombre;
 	private Zoologico zoo;
-	private Animal[] animal;
+	private ArrayList<Animal> animales;
 	
 	public Zona(){
 	}
 	
-	public Zona(String nombre, Zoologico zoo, Animal[] animal) {
+	public Zona(String nombre, Zoologico zoo, ArrayList<Animal> animales) {
 		this.nombre=nombre;
 		this.zoo=zoo;
-		this.animal=animal;
+		this.animales=animales;
+	}
+	
+	public void agregarAnimales() {
+		
+	}
+	
+	public int cantidadAnimales() {
+		return this.animales.size();
 	}
 }
--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,16 +1,34 @@
 package gestion;
 
+import java.util.ArrayList;
+
 public class Zoologico {
 	private String nombre;
 	private String ubicacion;
-	private Zona[] zonas;
+	private ArrayList<Zona> zonas;
 	
 	public Zoologico() {
 	}
 	
-	public Zoologico(String nombre, String ubicacion, Zona[] zonas) {
+	public Zoologico(String nombre, String ubicacion, ArrayList<Zona> zonas) {
 		this.nombre=nombre;
 		this.ubicacion=ubicacion;
 		this.zonas=zonas;
 	}
+
+	public void agregarZonas(Zona...zonas ) {
+		for (Zona zona:zonas) {
+			this.zonas.add(zona);
+		}
+		
+	}
+	
+	public int cantidadTotalAnimales() {
+		int contador = 0;
+		for(Zona zona: zonas) {
+			contador = contador + zona.cantidadAnimales();
+		}
+		return contador;
+	}
+	
 }
